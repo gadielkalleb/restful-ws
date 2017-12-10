889e1b960fca91b1170bb2dec555c88dcc530660
gadielkalleb <gadiel_kalleb@hotmail.com>
2017-12-10 20:25:21 -0200

    criacao dos metodos save all update e delete prontos

--- src/services/mysql/test/categories.test.js
+++ src/services/mysql/test/categories.test.js
@@ -3,9 +3,29 @@ const test = require('ava')
 const { connection, errorHandler } = require('./setup')
 const categories = require('../categories')({connection, errorHandler})
 
-test.beforeEach(t => connection.query('TRUNCATE TABLE categories'))
+// isolo o metodo create para usar ele isoladamente
+const create = () => categories.save('category-test')
 
+// limpa a tabela antes e dois do teste
+test.beforeEach(t => connection.query('TRUNCATE TABLE categories'))
+test.after.always(t => connection.query('TRUNCATE TABLE categories'))
+
+// salva um elemento dentro da tabela de testes
 test('Criação de categories', async t => {
-  const results = await categories.save('category-test')
+  const results = await create()
   t.is(results.category.name, 'category-test')
+})
+
+// teste que atualiza um dado da tabela
+test('Atualização de categories', async t => {
+  await create()
+  const update = await categories.update(1, 'category-test')
+  t.is(update.category.name, 'category-test')
+  t.is(update.affectedRows, 1)
+})
+
+test('Remoção de categories', async t => {
+  await create()
+  const removed = await categories.del(1)
+  t.is(removed.affectedRows, 1)
 })
